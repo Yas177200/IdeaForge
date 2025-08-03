b1df253f5157ad2be9d7a162838dd2e6d39feaa9
Yasser Abdulmala <90008483+Yas177200@users.noreply.github.com>
2025-08-04 00:00:58 +0200

    feature/auth-ui: simple ui to login and register

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h2>Protected Data</h2>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import api from "../Api"
+
+export default function Login() {
+    const [form, setForm] = useState({email: '', password: '', name: ''})
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+
+    const handleChange = e => 
+        setForm(f => ({...f, [e.target.name]: e.target.value}))
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        try{
+            const {data} = await api.post('/auth/login', form);
+
+            localStorage.setItem('token', data.token);
+            navigate('/');
+        }catch (err){
+            setError(err.response?.data?.message || 'Login failed');
+        }
+    };
+
+    return (
+        <div className="auth-container">
+            <h2>Login</h2>
+            {error && <p className="error" >{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <input name="email"     placeholder="Email"     type="email"        value={form.email}      onChange={handleChange} required/>
+                <input name="password"  placeholder="Password"  type="password"     value={form.password}   onChange={handleChange} required/>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
